86a3a9f60c78543475879a8a75cdef7fc4c4d91a
Владислав Г. <vlad65253@gmail.com>
2025-06-19 15:52:20 +0300

    создал основу репозитория

---
+++ src/main/java/com/test/gigacom/GigacomApplication.java
@@ -0,0 +1,13 @@
+package com.test.gigacom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigacomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GigacomApplication.class, args);
+	}
+
+}
